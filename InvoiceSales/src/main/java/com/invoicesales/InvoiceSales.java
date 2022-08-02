a19dc35f4e7b5583da31813058699faefd87b349
alaa mahmod <alaamahmod957@gmail.com>
2022-08-02 19:11:57 +0200

    commit first_commit

---
+++ InvoiceSales/src/main/java/com/invoicesales/InvoiceSales.java
@@ -0,0 +1,13 @@
+package com.invoicesales;
+ 
+
+public class InvoiceSales {
+
+    public static void main(String[] args) {
+        Frame frame =new Frame();
+        frame.setVisible(true);
+        
+ 
+        
+    }
+}
